bf89932ec597a37afa5d7ce01708eb12e2a35f7e
unknown <satnamdass25@gmail.com>
2026-09-24 22:01:16 +0530

    fixed error yaml file

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,33 +1,22 @@
-import Flutter
 import UIKit
-import FirebaseMessaging
+import Flutter
+import Firebase // Firebase के लिए
+import workmanager // Workmanager के लिए
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     
-    // 1. सबसे पहले Flutter के सभी प्लगइन्स को रजिस्टर करें
+    // Firebase को इनिशियलाइज़ करें (अगर आप Flutter कोड से नहीं कर रहे हैं)
+    // FirebaseApp.configure() 
+    
     GeneratedPluginRegistrant.register(with: self)
     
-    // 2. iOS 10+ के लिए पुश नोटिफिकेशन की परमिशन और डेलीगेट सेट करें
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-      UNUserNotificationCenter.current().requestAuthorization(
-        options: authOptions,
-        completionHandler: { _, _ in }
-      )
-    } else {
-      let settings: UIUserNotificationSettings =
-        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-      application.registerUserNotificationSettings(settings)
-    }
-
-    application.registerForRemoteNotifications()
+    // Workmanager टास्क रजिस्टर करें
+    WorkmanagerPlugin.registerBGProcessingTask(withIdentifier: "com.fixsathi.fixsathi.backgroundTask")
     
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
