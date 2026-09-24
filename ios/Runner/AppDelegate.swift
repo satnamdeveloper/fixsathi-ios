f06b20995ce162905583f0a0d4c09988b841f329
unknown <satnamdass25@gmail.com>
2026-09-24 22:27:52 +0530

    fixed error some files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import Flutter
 import UIKit
 import FirebaseMessaging
-import Workmanager // 'W' को कैपिटल (बड़ा) होना अनिवार्य है!
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,14 +10,10 @@ import Workmanager // 'W' को कैपिटल (बड़ा) होना 
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     
-    // 1. Flutter के सभी प्लगइन्स को रजिस्टर करें
+    // 1. Flutter के सभी प्लगइन्स को रजिस्टर करें (यह Workmanager को भी नेटिवली रजिस्टर कर देगा)
     GeneratedPluginRegistrant.register(with: self)
     
-    // 2. Workmanager के बैकग्राउंड टास्क को सही तरीके से रजिस्टर करें
-    // ध्यान दें: जो ID आपने Info.plist में डाली है, वही यहाँ होनी चाहिए
-    WorkmanagerPlugin.registerBGProcessingTask(withIdentifier: "com.fixsathi.fixsathi.backgroundTask")
-    
-    // 3. iOS 10+ के लिए पुश नोटिफिकेशन की परमिशन और डेलीगेट सेट करें
+    // 2. iOS 10+ के लिए पुश नोटिफिकेशन की परमिशन और डेलीगेट सेट करें
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
       let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
